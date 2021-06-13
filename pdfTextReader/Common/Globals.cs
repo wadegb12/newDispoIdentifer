40ff5b826a5ff862e9ba72af630d548f1ab30f54
kylenbills@gmail.com <kylenbills@gmail.com>
2021-06-12 23:19:52 -0500

    Removed WebDriver class and all refernces to it.

--- pdfTextReader/Common/Globals.cs
+++ pdfTextReader/Common/Globals.cs
@@ -89,20 +89,5 @@ namespace pdfTextReader.Common
         // KNB : 6/6/21 : Accpet string and post to console window.
         public static void LogIt(string logMessage) { Console.WriteLine("["+DateTime.Now.ToString()+ "] --- " + logMessage); }
 
-        // KNB : 6/6/Prefix the date to the newest file in the path.
-        public static void PrefixDateToLatestFile(WebDriver driver)
-        {
-            Globals.LogIt("Appending Date to newest file in the download directory.");
-            DirectoryInfo downloadDirectory = new DirectoryInfo(GetDailyFolder());
-            FileInfo newestFile = downloadDirectory.GetFiles().OrderByDescending(f => f.LastWriteTime).First();
-
-            string newName = DateTime.Now.ToString("yyyy-MM-dd") + " " + newestFile.Name;
-
-            newestFile.MoveTo(GetDailyFolder() + newName);
-
-            return;
-        }
-
-
     }
 }
