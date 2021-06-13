7494fdcbac5fa0221d7a32d952f80aea45eabedc
kylenbills@gmail.com <kylenbills@gmail.com>
2021-06-13 00:10:40 -0500

    Added more logging and list of removed dispensaries.

--- pdfTextReader/DispensaryIdentifier.cs
+++ pdfTextReader/DispensaryIdentifier.cs
@@ -14,6 +14,7 @@ namespace pdfTextReader
         public string TodaysOMMAListFile;
         public string TodaysDisposFile;
         public string TodaysNewDisposFile;
+        public string TodaysRemovedDisposFile;
         public Logger Logger;
 
         public DispensaryIdentifier()
@@ -30,29 +31,45 @@ namespace pdfTextReader
             Logger.WriteLog("Setting Local Variables");
 
             YesterdaysDisposFile = Globals.GetYesterdayFolder() + DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd") + " Licensed Dispos.txt";
+            TodaysDisposFile = Globals.GetDailyFolder() + DateTime.Now.ToString("yyyy-MM-dd") + " Licensed Dispos.txt";
 
             TodaysOMMAListFile = Globals.GetDailyFolder() + Globals.GetDailyDownloadFile();
-            TodaysDisposFile = Globals.GetDailyFolder() + DateTime.Now.ToString("yyyy-MM-dd") + " Licensed Dispos.txt";
             TodaysNewDisposFile = Globals.GetDailyFolder() + DateTime.Now.ToString("yyyy-MM-dd") + " New Dispos.txt";
+            TodaysRemovedDisposFile = Globals.GetDailyFolder() + DateTime.Now.ToString("yyyy-MM-dd") + " Removed Dispos.txt";
         }
 
         public void Run()
         {
             try
             {
+                Logger.WriteLog("Downloading todays dispensary list.");
                 DispensaryDownloader.DownloadNewDispensaries();
 
+                Logger.WriteLog("Creating output service and initializing the PDF reader.");
                 var outputService = new OutputService(Logger);
                 var dispoListReader = new DispensaryListReader(TodaysOMMAListFile, Logger);
 
+                Logger.WriteLog("Reading todays dispensaries from download file.");
                 var todaysDispos = dispoListReader.FindCompanies();
+                Logger.WriteLog("Outputting todays dispensaries to file.");
                 outputService.OutputTodaysCompanies(todaysDispos, TodaysDisposFile);
 
-                var yesterdaysDispos = ReadInYesterdaysDispos(YesterdaysDisposFile);
-                var yesterdaysDisposHashset = CreateNameHashSetFromDispensaryList(yesterdaysDispos);
+                if(File.Exists(Globals.GetYesterdayFolder() + Globals.GetYesterdayDownloadFile()))
+                {
+                    Logger.WriteLog("Yesterdays file exist, creating hashset of yesterdays dispensaries.");
+                    var yesterdaysDispos = ReadInYesterdaysDispos(YesterdaysDisposFile);
+                    var yesterdaysDisposHashset = CreateNameHashSetFromDispensaryList(yesterdaysDispos);
 
-                var newDispos = IdentifyNewDispos(todaysDispos, yesterdaysDisposHashset);
-                outputService.OutputNewDispos(newDispos, TodaysNewDisposFile);
+                    Logger.WriteLog("Identifying the new dispensaries and outputting them to file.");
+                    var newDispos = IdentifyNewDispos(todaysDispos, yesterdaysDisposHashset);
+                    outputService.OutputNewDispos(newDispos, TodaysNewDisposFile);
+
+                    Logger.WriteLog("Identifying the removed dispensaries and outputting them to file.");
+                    var todaysDispoHashset = CreateNameHashSetFromDispensaryList(todaysDispos);
+                    var removedDispos = IdentifyRemovedDispos(yesterdaysDispos, todaysDispoHashset);
+                    outputService.OutputRemovedDispos(removedDispos, TodaysRemovedDisposFile);
+                }
+                Logger.WriteLog("Application closing.");
             }
             catch (Exception e)
             {
@@ -101,6 +118,22 @@ namespace pdfTextReader
             }
 
             return newDispos;
+        }
+
+        public List<Dispensary> IdentifyRemovedDispos(List<Dispensary> yesterdaysDispos, HashSet<string> todaysDispos)
+        {
+            Logger.WriteLog("Identifying todays removed dispensaries.");
+            var removedDispos = new List<Dispensary>();
+
+            foreach (var dispo in yesterdaysDispos)
+            {
+                if (!todaysDispos.Contains(dispo.LicenseNum))
+                {
+                    removedDispos.Add(dispo);
+                }
+            }
+
+            return removedDispos;
         }
     }
 }
